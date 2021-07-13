10dfd4f8a1f813c2f6d4b984e339d37a8b1878f7
timfuhrmann <tf@becklyn.com>
2021-07-13 18:06:20 +0200

    Move font from global style

--- app/css/GlobalStyle.ts
+++ app/css/GlobalStyle.ts
@@ -1,11 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 import { reset } from "./reset";
-import { font } from "./font";
 import { FlowText } from "./typo";
 
 export const GlobalStyle = createGlobalStyle`
     ${reset};
-    ${font}
     
     body {
         font-family: "Circular Std", Helvetica, Arial, sans-serif;
